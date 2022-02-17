e913f5d30182e9df4bd02b36b8d5023582e357f0
Marciobob <marciobob47@gmail.com>
2022-02-17 20:10:53 -0300

    icines redes social

--- Botao_portao.js
+++ Botao_portao.js
@@ -20,20 +20,20 @@ function scrooll(){
   	
   	var tamanho_lista = document.querySelectorAll("img").length - 1
   	
-    const div = document.getElementById("faceboo")
+    //const div = document.getElementById("faceboo")
     
     if(index >1  && index < tamanho_lista && img.getBoundingClientRect().top < window.innerHeight){ 
   
       img.classList.add("img_servico2")
     }
-	else if(index == tamanho_lista  && img.getBoundingClientRect().top < window.innerHeight){ 
+	//else if(index == tamanho_lista  && img.getBoundingClientRect().top < window.innerHeight){ 
   		
-      div.classList.add("facebook2")
+     // div.classList.add("facebook2")
       
-    }
+   // }
     else{
 		img.classList.remove("img_servico2")
-		div.classList.remove("facebook2")
+		//div.classList.remove("facebook2")
     }
   })
 }
